46aa9d31c16b8e81c75d94f5853753a699f75df7
Heet kumar Kothadiya <heetkumarpatel@gmail.com>
2022-09-22 13:37:49 +0530

    Rightbar birthday and online friends components

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,24 @@ import './rightbar.css';
 export default function Rightbar(){
     return(
         <div className="rightbar">
-            Rightbar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="/assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText">
+                        <b>Hector</b> and <b>3 other friends</b> have a birthday today
+                    </span>
+                </div>
+                <img src="/assets/ad.jpg" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="/assets/person/3.jpg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
