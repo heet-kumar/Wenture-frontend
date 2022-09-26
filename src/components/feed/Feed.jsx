f9ee146d00d4cdfc0318909c6869ec5776aeaf1f
Heet kumar Kothadiya <heetkumarpatel@gmail.com>
2022-09-27 01:11:43 +0530

    Post component implemented with Dummy data

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import Post from '../post/Post';
 import Share from '../share/Share';
 import './feed.css';
+import {Posts} from '../../dummyData';
 
 export default function Feed(){
     return(
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                {/* <Post /> */}
+                {
+                    Posts.map(p => {
+                        return <Post key={p.id} value={p} />
+                    })
+                }
             </div>
         </div>
     );
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
 import './post.css';
-import {MoreVert} from "@mui/icons-material"
+import {MoreVert} from "@mui/icons-material";
+import {Users} from "../../dummyData";
 
-export default function Post(){
+const Post = ({value}) => {
+    console.log(value);
+    const user = Users.filter(u => u.id===value.userId);    // like this we call data for the user from map
+    console.log(user)
     return(
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img className="postProfileImg" src="/assets/person/1.jpg" alt=""/>
-                        <span className="postUserName">Heet Kumar</span>
-                        <span className="postDate">5 mins ago</span>
+                        <img className="postProfileImg" src={Users.filter(u => u.id===value.userId)[0].profilePicture} alt=""/>
+                        <span className="postUserName">{Users.filter(u => u.id===value.userId)[0].username}</span>
+                        <span className="postDate">{value.date}</span>
                     </div>
                     <div className="postTopRight">
                         <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter">
-                    <span className="postText">Hey! its my first post :)</span>
-                    <img src="/assets/post/p4.jpg" alt="" className="postImg" />
+                    <span className="postText">{value?.desc}</span>
+                    <img src={value.photo} alt="" className="postImg" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img src="/assets/like.jpeg" alt="" className="likeIcon" />
                         <img src="/assets/heart.png" alt="" className="likeIcon" />
-                        <span className="likeCounter">32 people liked it!</span>
+                        <span className="likeCounter">{value.like} people liked it!</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">9 comments</span>
+                        <span className="postCommentText">{value.comment} comments</span>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
+
+export default Post;
