1adb04d517c702b96106cba815d83e216504c269
Heet kumar Kothadiya <heetkumarpatel@gmail.com>
2022-10-07 19:00:05 +0530

    Profile page relocated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Profile from './pages/profile/Profile';
 import Home from './pages/home/Home';
 
 
 function App() {
   return (
-    <Home />
+    // <Home />
+    <Profile />
   );
 }
 
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -4,7 +4,7 @@ import './profile.css';
 const Profile = () => {
     return(
         <div>
-            Profile
+            Profile page
         </div>
     );
 }
